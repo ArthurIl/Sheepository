76e18adabae828f588a6a2716fa7fedf453f70d4
Unknown <orelien.mary@gmail.com>
2021-02-24 17:26:25 +0100

    gitflow-feature-stash: characterController

---
+++ Sheep Project/Assets/Scripts/InputListener.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Aurélien
+/// </summary>
+
+public class InputListener : MonoBehaviour
+{
+    public static InputListener iL;
+
+   public  Vector2 touchPositionBegin;
+
+    public Vector2 swipeDirection;
+
+    public bool longSwipe = false;
+    float swipeSince = 0;
+
+    private void Awake()
+    {
+        iL = this;
+    }
+
+    private void Update()
+    {
+        if(Input.touches.Length == 0)
+        {
+            touchPositionBegin = Vector2.zero;
+            swipeDirection = Vector2.zero;
+            swipeSince = 0;
+            longSwipe = false;
+            return;
+        }
+
+        if(Input.touches[0].phase == TouchPhase.Began)
+        {
+            touchPositionBegin = Input.touches[0].position;
+        }
+
+        if(Input.touches[0].phase == TouchPhase.Moved || Input.touches[0].phase == TouchPhase.Stationary)
+        {
+            swipeDirection = (Input.touches[0].position - touchPositionBegin).normalized;
+
+            swipeSince += Time.deltaTime;
+
+            if(swipeSince > 1f)
+            {
+                longSwipe = true;
+            }
+        }
+
+        if(Input.touches[0].phase == TouchPhase.Ended)
+        {
+            touchPositionBegin = Vector2.zero;
+            swipeDirection = Vector2.zero;
+            swipeSince = 0;
+            longSwipe = false;
+        }
+    }
+}
---
+++ Sheep Project/Assets/Scripts/PMovementController.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Aurélien
+/// </summary>
+public class PMovementController : MonoBehaviour
+{
+    bool moving = false;
+
+    public LayerMask blocMask;
+    public LayerMask sheepMask;
+
+    public float normalMovementTime;
+
+    void Update()
+    {
+        if(InputListener.iL.swipeDirection != Vector2.zero)
+        {
+            if (moving == false)
+                OneMovement(InputListener.iL.swipeDirection);
+        }
+    }
+
+    void OneMovement(Vector2 direction)
+    {
+        moving = true;
+
+        float directionAngle = Vector2.Angle(Vector2.right, direction);
+
+        if (direction.y < 0)
+            directionAngle = -directionAngle;
+
+        if (-45 < directionAngle && directionAngle <= 45)
+            direction = Vector2.right;
+        else if (45 < directionAngle && directionAngle <= 135)
+            direction = Vector2.up;
+        else if (135 < directionAngle || directionAngle <= -135)
+            direction = Vector2.left;
+        else if (-135 < directionAngle && directionAngle <= -45)
+            direction = Vector2.down;
+
+        Vector3 directionConverted = new Vector3(direction.x, 0f, direction.y);
+
+        DetectAroundInDirection(directionConverted);
+    }
+
+    void DetectAroundInDirection(Vector3 direction)
+    {
+        if(Physics.Raycast(transform.position, direction, 1f, blocMask))
+        {
+            StartCoroutine(BonkAgainstWall(direction));
+        }
+        else if(Physics.Raycast(transform.position + direction, Vector3.down, 1f, blocMask))
+        {
+            StartCoroutine(NormalMove(direction));
+        }
+        else
+        {
+            StartCoroutine(MoveThenFall(direction));
+        }
+        
+        if(Physics.Raycast(transform.position, direction, 1f, sheepMask))
+        {
+            Debug.Log("DontMove");
+        }
+
+    }
+
+    IEnumerator NormalMove(Vector3 direction)
+    {
+        for(float i = 0; i < normalMovementTime; i += Time.fixedDeltaTime)
+        {
+            transform.position += direction * Time.fixedDeltaTime / normalMovementTime;
+
+            yield return new WaitForFixedUpdate();
+        }
+
+        moving = false;
+    }
+
+    IEnumerator BonkAgainstWall(Vector3 direction)
+    {
+        for (float i = 0; i < 0.12f; i += Time.fixedDeltaTime)
+        {
+            transform.position += direction * Time.fixedDeltaTime / normalMovementTime;
+
+            yield return new WaitForFixedUpdate();
+        }
+
+        for (float i = 0; i < 0.12f; i += Time.fixedDeltaTime)
+        {
+            transform.position -= direction * Time.fixedDeltaTime / normalMovementTime;
+
+            yield return new WaitForFixedUpdate();
+        }
+
+        moving = false;
+    }
+
+    IEnumerator MoveThenFall(Vector3 direction)
+    {
+        for (float i = 0; i < normalMovementTime; i += Time.fixedDeltaTime)
+        {
+            transform.position += direction * Time.fixedDeltaTime / normalMovementTime;
+
+            yield return new WaitForFixedUpdate();
+        }
+
+        StartCoroutine(FallUntilSomething());
+    }
+
+    IEnumerator FallUntilSomething()
+    {
+        for (float i = 0; i < normalMovementTime/2f; i += Time.fixedDeltaTime)
+        {
+            transform.position += Vector3.down * Time.fixedDeltaTime / (normalMovementTime/2f);
+
+            yield return new WaitForFixedUpdate();
+        }
+
+        if (Physics.Raycast(transform.position, Vector3.down, 1f, blocMask))
+        {
+            moving = false;
+        }
+        else
+        {
+            StartCoroutine(FallUntilSomething());
+        }
+    }
+}
