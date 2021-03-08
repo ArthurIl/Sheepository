22dc23824d7b055b893e2dcfd7753d03ca946190
Aurélien <orelien.mary@gmail.com>
2021-03-08 12:32:28 +0100

    Level Changer for Build Pre Prod

---
+++ Sheep Project/Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Aurélien
+/// </summary>
+public class LevelChanger : MonoBehaviour
+{
+    public static LevelChanger lc; 
+
+    void Awake()
+    {
+        if (lc != null)
+        {
+            Destroy(this.gameObject);
+        }
+        {
+            lc = this;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void Previous()
+    {
+        int currentSceneBuildIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if(currentSceneBuildIndex != 0)
+        {
+            SceneManager.LoadScene(currentSceneBuildIndex - 1);
+        }
+    }
+
+    public void Next()
+    {
+        int currentSceneBuildIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if (currentSceneBuildIndex != (SceneManager.sceneCountInBuildSettings-1))
+        {
+            SceneManager.LoadScene(currentSceneBuildIndex + 1);
+        }
+    }
+}
